a5d7d704c756bfe6151b1d008315ec38e0dad696
yinruei <yinruei@cloudybay.com.tw>
2019-01-15 18:19:46 +0800

    update

--- ch14/ajax-admin/all.js
+++ ch14/ajax-admin/all.js
@@ -1,58 +1,124 @@
-var regist = document.querySelector('.register');
+// var regist = document.querySelector('.register');
+// regist.addEventListener('click',signup,false);
+
+// function signup(){
+//     var emailStr = document.querySelector('.account').value;
+//     var passwordStr = document.querySelector('.password').value;
+//     var account = {};
+//     account.email = emailStr;
+//     account.password = passwordStr;
+
+//     var xhr = new XMLHttpRequest();
+//     xhr.open('post','https://hexschool-tutorial.herokuapp.com/api/signup',true);
+//     xhr.setRequestHeader('Content-type','application/json');
+//     var data = JSON.stringify(account);  // {}==>"{}"
+//     // console.log(typeof(data))
+//     xhr.send(data)
+
+//     xhr.onload = function(){
+//         // console.log(xhr);
+//         var callbackData = JSON.parse(xhr.responseText);  //"[{}]" ==>>[{}]
+//         console.log(callbackData);
+//         var verify = callbackData.message;
+//         if (verify == '帳號註冊成功'){
+//             alert('帳號註冊成功!!!');
+//         }else{
+//             alert('此帳號已被使用!!');
+//         }
+//     }
+// }
+
+// var login = document.querySelector('.login');
+// login.addEventListener('click',signin,false);
+
+// function signin(){
+//     var emailStr = document.querySelector('.account').value;
+//     var passwordStr = document.querySelector('.password').value;
+//     var account = {};
+//     account.email = emailStr;
+//     account.password = passwordStr;
+
+//     var xhr = new XMLHttpRequest();
+//     xhr.open('post','https://hexschool-tutorial.herokuapp.com/api/signin',true);
+//     xhr.setRequestHeader('Content-type','application/json');
+//     var data = JSON.stringify(account);
+//     xhr.send(data)
+
+//     xhr.onload = function(){
+//         console.log(xhr);
+//         var callbackData = JSON.parse(xhr.responseText);
+//         console.log(callbackData);
+//         var verify = callbackData.message;
+//         if (verify == '登入成功'){
+//             alert('登入成功!!!');
+//         }else{
+//             alert('此帳號不存在或帳號密碼錯誤');
+//         }
+//     }
+// }
+
+
+let regist = document.querySelector('.register');
 regist.addEventListener('click',signup,false);
 
 function signup(){
-    var emailStr = document.querySelector('.account').value;
-    var passwordStr = document.querySelector('.password').value;
-    var account = {};
+    let emailStr = document.querySelector('.account').value;
+    let passwordStr = document.querySelector('.password').value;
+    let account = {};
     account.email = emailStr;
     account.password = passwordStr;
+    console.log(account)
 
-    var xhr = new XMLHttpRequest();
-    xhr.open('post','https://hexschool-tutorial.herokuapp.com/api/signup',true);
-    xhr.setRequestHeader('Content-type','application/json');
-    var data = JSON.stringify(account);  // {}==>"{}"
-    // console.log(typeof(data))
-    xhr.send(data)
-
-    xhr.onload = function(){
-        console.log(xhr);
-        var callbackData = JSON.parse(xhr.responseText);  //"[{}]" ==>>[{}]
-        console.log(callbackData);
-        var verify = callbackData.message;
-        if (verify == '帳號註冊成功'){
-            alert('帳號註冊成功!!!');
-        }else{
-            alert('此帳號已被使用!!');
-        }
+    let url_up = 'https://hexschool-tutorial.herokuapp.com/api/signup';
+    fetch(url_up, {
+    method: 'post',
+    // headers 加入 json 格式
+    headers: {
+        'Content-Type': 'application/json'
+    },
+    // body 將 json 轉字串送出
+    body: JSON.stringify(account)
+    }).then(function(response){
+        console.log(response);
+        return response.json();
+        })
+        .then(function(data){
+        console.log(data);      
+        })
+        .catch(function(err){
+        console.log('錯誤',err);
+        })
     }
-}
 
-var login = document.querySelector('.login');
+console.log('test')
+
+const login = document.querySelector('.login');
 login.addEventListener('click',signin,false);
 
 function signin(){
-    var emailStr = document.querySelector('.account').value;
-    var passwordStr = document.querySelector('.password').value;
-    var account = {};
+    const emailStr = document.querySelector('.account').value;
+    const passwordStr = document.querySelector('.password').value;
+    const account = {};
     account.email = emailStr;
     account.password = passwordStr;
+    console.log(account)
 
-    var xhr = new XMLHttpRequest();
-    xhr.open('post','https://hexschool-tutorial.herokuapp.com/api/signin',true);
-    xhr.setRequestHeader('Content-type','application/json');
-    var data = JSON.stringify(account);
-    xhr.send(data)
-
-    xhr.onload = function(){
-        console.log(xhr);
-        var callbackData = JSON.parse(xhr.responseText);
-        console.log(callbackData);
-        var verify = callbackData.message;
-        if (verify == '登入成功'){
-            alert('登入成功!!!');
-        }else{
-            alert('此帳號不存在或帳號密碼錯誤');
-        }
+    let url_in = 'https://hexschool-tutorial.herokuapp.com/api/signin';
+    fetch(url_in, {
+    method: 'post',
+    // headers 加入 json 格式
+    headers: {
+        'Content-Type': 'application/json'
+    },
+    body: JSON.parse(account)//"[{}]" ==>>[{}]
+    }).then(function(response){
+        console.log(response);
+        return response.json();
+        })
+        .then(function(data){
+        console.log(data);      
+        })
+        .catch(function(err){
+        console.log('錯誤', err);
+        })
     }
-}
